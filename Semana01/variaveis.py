c7b5ead2baa3037d772a5a9b013984e3ef3a892e
samuelbley <toniassosamuel@gmail.com>
2026-03-10 22:13:11 -0300

    Update variaveis.py

--- Semana01/variaveis.py
+++ Semana01/variaveis.py
@@ -1,8 +1,9 @@
+#hello, my name is samuel and i want go to my home sleepy#
 nome = "Samuel"
 idade = 18
 peso = 72.5
 
 print("Nome: ", nome)
-print("Idade: ", idade)
-print("Peso: ", peso)
+print("Idade: ", idade, "anos")
+print("Peso: ", peso, "Kg")
 print("Hello Word")
